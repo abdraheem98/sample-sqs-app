3886db2a7b6488442aa46768ee9678a96b8978b7
Abdul Raheem <139868961+abdraheem98@users.noreply.github.com>
2024-02-13 16:25:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,5 +33,7 @@ def send_message():
         Message=message
     )
 
+    return 'Message sent successfull'
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
